82c69d02729f0116c99cb47e0573f48f0e9830c6
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2021-05-21 00:10:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace drawashape
+{
+    class Program
+    {
+        //mad lib game
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celeb;
+            
+            Console.WriteLine("Enter a color : ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun : ");
+            pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Enter a celeb : ");
+            celeb = Console.ReadLine();
+
+            Console.WriteLine("Roses are {0}",color);
+            Console.WriteLine("{0} are blue", pluralNoun);
+            Console.WriteLine("I Love {0}", celeb);
+            
+            Console.ReadLine();
+        }
+    }
+}
